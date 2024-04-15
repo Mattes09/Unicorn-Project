6923ba05647e4053a09379ec3ca7fb2861708688
Mattes09 <111385032+Mattes09@users.noreply.github.com>
2024-04-15 23:41:23 +0200

    Minor Changes
    Minor changes such as typos fixed, error handling added to GET method for listing user. Listing all users functionality added. AJV Jason schema validator dependancy added.

--- backend/src/components/user/user.service.js
+++ backend/src/components/user/user.service.js
@@ -1,16 +1,26 @@
-const database = require("../../database/databse");
+const database = require("../../database/database");
 const { UserDAO } = require("./user.model");
 
 const UserService = {
   getUser: (req, res) => {
     const userId = req.params.id;
     const userDao = new UserDAO(database);
-
     const foundUser = userDao.getUser(userId);
+    if (!foundUser) {
+      return res.status(404).send({ error: "User not found" });
+    }
 
     res.send({
       user: foundUser,
     });
   },
 };
-module.exports = UserService;
+
+const listUsers = (req, res) => {
+  res.send(database);
+};
+
+module.exports = {
+  UserService,
+  listUsers,
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const userRouter = require("./components/user/user.routes");
+const cors = require("cors");
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); //application json support
+app.use(express.urlencoded({ extended: true })); //support for application/x-www-form-urlencoded
+app.use(cors()); //Cross-Origin Resource Sharing, managing requests from different domains
 
-app.use("/user", userRouter);
+app.use("/users", userRouter);
 
-app.get("/user/hello-world", (req, res) => {
-  //logika co se deje
-  res.send("Všetko funguje !");
+app.get("/", (req, res) => {
+  res.send("Don't smoke!");
+});
+
+app.post("/updatePage", (req, res) => {
+  if (req.body.id === "1") {
+    res.send("Page updated successfully");
+  } else if (req.body.id === "2") {
+    return res.status(400).send("page not updated");
+  } else {
+    return res.status(500).send("server error");
+  }
 });
 
 app.listen(PORT, () => {
